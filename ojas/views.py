d2cf91c8a261910a818181d73edaa06bf7fe1901
ojas <ojasok@gmail.com>
2016-11-22 18:08:31 -0600

    Fetching generic movie

--- ojas/views.py
+++ ojas/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def index(req):
-    return render(req, 'personal/home.html')
+    resp = requests.get('http://www.omdbapi.com/?s=harry potter')
+
+    for x in resp.json()['Search']:
+        for k,v in x.iteritems():
+            print k+ " " +v
+
+
+
+    return render(req, 'personal/home.html',{'movies':resp.json()['Search']})
+
 
 # Create your views here.
