e294437abc5b924e98920264909c50906aeff210
ojas <ojasok@gmail.com>
2016-11-23 01:10:15 -0600

    Getting aall the movies Properly

--- ojas/views.py
+++ ojas/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
 import requests
 
 def index(req):
-    resp = requests.get('http://www.omdbapi.com/?s=harry potter')
+    if req.method == 'GET':
+        resp = requests.get('http://www.omdbapi.com/?s=harry potter&type=movie')
+        return render(req, 'personal/home.html', {'movies': resp.json()['Search']})
 
-    for x in resp.json()['Search']:
-        for k,v in x.iteritems():
-            print k+ " " +v
+    if req.method == 'POST':
+
+        mov = req.POST.get('conf')
+        print mov
+        if(mov):
+            string = 'http://www.omdbapi.com/?s='+mov+'&type=movie'
+            resp = requests.get(string)
+            print resp
+            if(resp.json()['Response']=='False'):
+                return render(req, 'personal/home.html', {'movies': ''})
+            else:
+                return render(req, 'personal/home.html', {'movies': resp.json()['Search']})
+
+        else:
+            string = 'http://www.omdbapi.com/?s=spider&type=movie'
+            resp = requests.get(string)
+            return render(req, 'personal/home.html', {'movies': resp.json()['Search']})
 
 
 
-    return render(req, 'personal/home.html',{'movies':resp.json()['Search']})
-
-
-# Create your views here.
